f314d4e5e4a7f4d811c01c1cc21ff79759d37109
Sehyun Kim <mathshyun@gmail.com>
2025-09-09 20:37:51 +0900

    Add SequenceData

---
+++ Assets/Scripts/Sequence/SequenceData.cs
@@ -0,0 +1,11 @@
+﻿namespace Sequence
+{
+    public abstract class SequenceData
+    {
+        public string Name;
+
+        public float TimingSeconds;
+        public float CorrectTimingSeconds;
+        public float DelayAfterSequenceSeconds;
+    }
+}
