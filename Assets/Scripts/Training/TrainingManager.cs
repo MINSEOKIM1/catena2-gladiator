b7bebfbeb51825b36ef436a9ef61438da23de28e
Sehyun Kim <mathshyun@gmail.com>
2025-10-21 18:47:37 +0900

    Add training

---
+++ Assets/Scripts/Training/TrainingManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Training
+{
+    public class TrainingManager : MonoBehaviour
+    {
+        [Header("UI")]
+        [SerializeField] private Button attackTrainingButton;
+        [SerializeField] private Button defenseTrainingButton;
+        [SerializeField] private Button quitButton;
+        [SerializeField] private TMP_Text statusText;
+
+        [Header("Character")]
+        [SerializeField] private SpriteRenderer character;
+
+        [Space(10)]
+        [SerializeField] private Sprite characterIdleSprite;
+        [SerializeField] private Sprite characterTrainingAttackSprite;
+        [SerializeField] private Sprite characterTrainingDefenseSprite;
+
+        private void SetButtonsInteractable(bool value)
+        {
+            attackTrainingButton.interactable = value;
+            defenseTrainingButton.interactable = value;
+            quitButton.interactable = value;
+        }
+
+        private IEnumerator PerformTraining()
+        {
+            const float trainingDuration = 3f;
+            const float trainingStatusDisplayDuration = 1f;
+            
+            // TODO: Add character training animation
+            
+            yield return new WaitForSeconds(trainingDuration);
+
+            character.sprite = characterIdleSprite;
+            SetButtonsInteractable(true);
+            statusText.gameObject.SetActive(true);
+
+            yield return new WaitForSeconds(trainingStatusDisplayDuration);
+            
+            statusText.gameObject.SetActive(false);
+        }
+        
+        public void TrainAttack()
+        {
+            SetButtonsInteractable(false);
+            
+            // TODO: Determine training outcome value
+            const int atkIncrease = 15;
+            // TODO: Apply training outcome
+
+            statusText.text = $"공격력이 {atkIncrease} 증가했습니다!";
+            character.sprite = characterTrainingAttackSprite;
+            
+            StartCoroutine(PerformTraining());
+        }
+        
+        public void TrainDefense()
+        {
+            SetButtonsInteractable(false);
+            
+            // TODO: Determine training outcome value
+            const int defIncrease = 15;
+            // TODO: Apply training outcome
+
+            statusText.text = $"방어력이 {defIncrease} 증가했습니다!";
+            character.sprite = characterTrainingDefenseSprite;
+            
+            StartCoroutine(PerformTraining());
+        }
+
+        public void QuitTraining()
+        {
+            SceneManager.LoadScene("Scenes/Map");
+        }
+    }
+}
