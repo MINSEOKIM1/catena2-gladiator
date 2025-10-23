76a9ebb5747d15bd32ebd40261bbd83a6ac3bf7d
Jeon, Minseo <minseo4565@snu.ac.kr>
2025-10-24 02:31:44 +0900

    UI ADD 1

---
+++ Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    public Image fadeImage;
+    public float timeToFadeIn = 5f;
+
+    public void OnStart()
+    {
+        StartCoroutine(SceneChange());
+    }
+
+    private IEnumerator SceneChange()
+    {
+        fadeImage.gameObject.SetActive(true);
+
+        while (fadeImage.color.a < 1f)
+        {
+            fadeImage.color = Color.Lerp(fadeImage.color, Color.black, timeToFadeIn * Time.deltaTime);
+            yield return null;
+        }
+        
+        SceneManager.LoadScene("BucketScene");
+    }
+}
